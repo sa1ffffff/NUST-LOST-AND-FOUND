13e2e38f24af1ceb7271be68ad916db64ff00547
Saifullah Waseem <saifullahwasim1@gmail.com>
2025-12-12 01:07:29 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"; // <--- ADDED IMPORT
 import Index from "./pages/Index";
 import Found from "./pages/Found";
 import Lost from "./pages/Lost";
@@ -28,6 +29,9 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      
+      <Analytics /> {/* <--- ADDED COMPONENT HERE */}
+      
     </TooltipProvider>
   </QueryClientProvider>
 );
